3ec9382169f012157c44ff8b0f9763020fd3babd
synoize <suryavanshivam@gamil.com>
2025-09-28 15:10:51 +0530

    cors added

--- server/server.js
+++ server/server.js
@@ -16,6 +16,8 @@ const corsOptions = {
     origin: [
         "http://localhost:5174",
         "http://localhost:5173",
+        "https://printova.vercel.app",
+        "https://printova-admin.vercel.app"
     ],
     methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
     credentials: true,
